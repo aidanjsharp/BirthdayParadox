45698a06614cd9ee193b11836978c8bdab9f24ba
aidanjsharp <aidanjsharp@hotmail.com>
2021-09-03 12:15:08 -0400

    basic numpy random

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from numpy import random
+
+vals = random.standard_normal(10)
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(random.standard_normal(10))
 
 
 # Press the green button in the gutter to run the script.
