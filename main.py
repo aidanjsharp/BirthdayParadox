be208998ec6c044a3ef176977b3a1847d4358d5e
aidanjsharp <aidanjsharp@hotmail.com>
2021-09-07 14:51:57 -0400

    complete simulation 1x

--- main.py
+++ main.py
@@ -4,13 +4,26 @@ from numpy .random import randint
 
 def main():
     # Use a breakpoint in the code line below to debug your script.
+    days = []
+    matched = False
+    while matched == False:
+        rDay = randDay()
+        matched = checkDay(rDay, days)
+        days.append(rDay)
+
+    print("Found a match on day", rDay, "!")
+    print("days:")
+    print(days)
+
+    tries = len(days)
+    print("This took", tries, "tries.")
 
 
-
-
-    print(randDay())
-
-
+def checkDay(rDay,days):
+    for day in days:
+        if day == rDay:
+            return True
+    return False
 
 def randDay():
     return randint(1, 366)
